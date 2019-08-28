046a57470135da879ccbaa2af3dab0678279d171
w8l9in3 <extern.hari.parkash@volkswagen.de>
2019-08-28 16:54:59 +0200

    upgrade to 12.7 and initialize backend

--- deploy.sh
+++ deploy.sh
@@ -3,51 +3,88 @@ set -e
 
 
 if ! [ -x "$(command -v python3)" ]; then
-  echo 'Error: python3 is not installed.' >&2
+  echo 'Error: python3 is not installed.'
+  exit 1
+fi
+
+if ! [ -x "$(command -v awk)" ]; then
+  echo 'Error: awk is not installed.'
   exit 1
 fi
 
 if ! [ -x "$(command -v jq)" ]; then
-  echo 'Error: jq is not installed.' >&2
+  echo 'Error: jq is not installed.'
   exit 1
 fi
 
 if ! [ -x "$(command -v terraform)" ]; then
-  echo 'Error: terraform not available!.' >&2
+  echo 'Error: terraform not available!.'
   exit 1
 fi
 
 if ! [ -x "$(command -v ansible-playbook)" ]; then
-  echo 'Error: ansible-playbook not available!.' >&2
+  echo 'Error: ansible-playbook not available!.'
   exit 1
 fi
 
 if ! [ -x "$(command -v ssh-keygen)" ]; then
-  echo 'Error: ssh-keygen not available!.' >&2
+  echo 'Error: ssh-keygen not available!.'
   exit 1
 fi
 
 if ! [ -x "$(command -v aws)" ]; then
-  echo 'Error: aws not available!.' >&2
+  echo 'Error: aws not available!.'
   exit 1
 fi
 
+pushd () {
+    command pushd "$@" > /dev/null
+}
+
+popd () {
+    command popd "$@" > /dev/null
+}
 
 if [[ -z "$AWS_ACCESS_KEY_ID" && ! -f "$HOME/.aws/credentials" ]]
 then
     aws configure
 fi
 
+read -p "Enter Stage Name (dev or test): " STAGE
+
+pushd terraform
+
+[ -z $STAGE ] && { echo "STAGE parameter is empty. Exiting..." ; exit 1; }
+[ ! -d vars/$STAGE ] && { echo "No directory found for specified stage: $STAGE" ; exit 1; }
+
+
+BACKEND_VARS_PATH="$(pwd)/vars/$STAGE/backend.tfbackend"
+INFRA_VARS_PATH="$(pwd)/vars/$STAGE/infra.tfvars"
+
+S3_BUCKET="$(sed -n 's/^bucket\s*=\s*"\(.*\)"$/\1/p' < $BACKEND_VARS_PATH)"
+
+if [[ -z "${S3_BUCKET}" ]]; then
+  echo "please define backend bucket in $BACKEND_VARS_PATH"
+  exit 1
+fi
+
+
+if  aws s3api head-bucket --bucket $S3_BUCKET 2>&1 ; then
+    echo "bucket already exist and accessible"
+else
+    echo "bucket does not exit or permission is not there to view it, creating bucket"
+    pushd backend
+    terraform init && terraform apply -auto-approve -input=false -refresh=true -var-file=$BACKEND_VARS_PATH || true
+    popd
+fi
 
 
 
-BACKEND_VARS_PATH="$(pwd)/vars/backend.tfbackend"
-INFRA_VARS_PATH="$(pwd)/vars/infra.tfvars"
-
-terraform init && terraform apply -auto-approve -input=false -refresh=true -var-file=$BACKEND_VARS_PATH || true
 
 terraform init -input=false -backend=true -backend-config=$BACKEND_VARS_PATH
 [ $? -ne 0 ] && { echo "Execution failed! Exiting..."; exit 1; }
+
+terraform import aws_s3_bucket.tfstate i-abcd1234
 
 terraform apply -auto-approve -input=false -refresh=true -var-file=$INFRA_VARS_PATH
 [ $? -ne 0 ] && { echo "Execution failed! Exiting..."; exit 1; }
--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,24 +1,6 @@
-resource "aws_s3_bucket" "tfstate" {
-  bucket = "${var.bucket}"
-  acl    = "private"
-
-  region = "${var.region}"
-
-  versioning {
-    enabled = true
-  }
-
-  force_destroy = true
-}
-
-resource "aws_dynamodb_table" "tfstate_lock" {
-  name           = "${var.dynamodb_table}"
-  hash_key       = "LockID"
-  read_capacity  = 10
-  write_capacity = 10
-
-  attribute {
-    name = "LockID"
-    type = "S"
+terraform {
+  required_version = "0.12.7"
+  backend "s3" {
   }
 }
+
